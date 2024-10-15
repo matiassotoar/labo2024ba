605582ca067044f24cdd2bbede731a8a7e4fe1fb
matiassotoar <matiassoto.ar@gmail.com>
2024-10-15 21:18:52 +0000

    auto commit 9       20241015 211846

--- src/workflows/529_workflow_base_f202109_viv001.r
+++ src/workflows/529_workflow_base_f202109_viv001.r
@@ -429,8 +429,8 @@ wf_septiembre <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
-  #DT_incorporar_dataset_competencia2024()
-  CA_catastrophe_base( metodo="MachineLearning")
+  DT_incorporar_dataset_competencia2024()
+  #CA_catastrophe_base( metodo="MachineLearning")
   FEintra_manual_base()
   DR_drifting_base(metodo="rank_simple")
   FEhist_base()
